93e18a4d530256c45c89a7f16838faa4365e7220
Noppawit <noppawit.han@gmail.com>
2025-07-17 03:22:05 +0700

    Add redux and config

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,18 +1,33 @@
 "use client";
 
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsSidebarCollapsed } from "@/app/state";
+
 import { Menu } from 'lucide-react';
 import React from 'react'
 
 
 const Sidebar = () => {
-    return (
-        <div>
-            {/* TOP LOGO */}
-            <div className={`flex gap-3 justify-between md:justify-normal items-center pt-8`}>
-                <div>logo</div>
-                <h1 className="font-extrabold text-2xl">MYSTOCK</h1>
+    const dispatch = useAppDispatch();
+    const isSidebarCollapsed = useAppSelector(
+        (state) => state.global.isSidebarCollapsed
+    );
 
-                <button className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" onClick={() => { }}>
+    const toggleSidebar = () => {
+        dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+    };
+
+    const sidebarClassNames = `fixed flex flex-col ${isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"
+        } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`;
+
+    return (
+        <div className={sidebarClassNames}>
+            {/* TOP LOGO */}
+            <div className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${isSidebarCollapsed ? "px-5" : "px-8"}`}>
+                <div>logo</div>
+                <h1 className={`${isSidebarCollapsed ? "hidden" : "block"} font-extrabold text-2xl`}>MYSTOCK</h1>
+
+                <button className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" onClick={toggleSidebar}>
                     <Menu className='w-4 h-4' />
                 </button>
             </div>
